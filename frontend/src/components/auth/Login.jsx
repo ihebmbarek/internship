a02dd3b8bfbb97081b31f648c56186efd464c8ee
Udgeet <bhatt8udgeet@gmail.com>
2025-01-02 14:15:19 +0530

    Setup Redux Toolkit for State Management Done.

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -3,45 +3,51 @@ import { Label } from "@radix-ui/react-label";
 import { Input } from "../ui/input";
 import { RadioGroup } from "@/components/ui/radio-group";
 import { Button } from "../ui/button";
-import { Link ,useNavigate  } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
 import { USER_API_END_POINT } from "@/utils/constant";
-import { toast } from "sonner"
+import { toast } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "@/redux/authSlice";
+import { Loader2 } from "lucide-react";
 
 const Login = () => {
   const [input, setInput] = useState({
-    email:"",
-    password:"",
-    role:""
+    email: "",
+    password: "",
+    role: "",
   });
-
+  const { loading } = useSelector((store) => store.auth);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const ChangeEventHandler = (e)=>{
-    setInput({...input, [e.target.name]:e.target.value});
-  }
+  const ChangeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
 
-  const LoginSubmitHandler = async(e) =>{
+  const LoginSubmitHandler = async (e) => {
     e.preventDefault();
     console.log(input);
     try {
-      const res = await axios.post(`${USER_API_END_POINT}/login`,input, {
-        headers:{
-          'Content-Type':'application/json' 
+      dispatch(setLoading(true));
+      const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+        headers: {
+          "Content-Type": "application/json",
         },
-        withCredentials:true,
+        withCredentials: true,
       });
-      if(res.data.success){
-        navigate('/');
+      if (res.data.success) {
+        navigate("/");
         toast.success(res.data.message);
       }
     } catch (error) {
-      console.log(error)
-      toast.error(error.response.data.message)
+      console.log(error);
+      toast.error(error.response.data.message);
+    } finally {
+      dispatch(setLoading(false));
     }
-
-  }
+  };
 
   return (
     <div>
@@ -54,26 +60,67 @@ const Login = () => {
           <h1 className="font-bold text-xl mb-5">Login</h1>
           <div className="my-2">
             <Label>Email</Label>
-            <Input type="email" name="email" value={input.email}  onChange={ChangeEventHandler} placeholder="udgeetbhatt271@gmail.com" />
+            <Input
+              type="email"
+              name="email"
+              value={input.email}
+              onChange={ChangeEventHandler}
+              placeholder="udgeetbhatt271@gmail.com"
+            />
           </div>
           <div className="my-2">
             <Label>Password</Label>
-            <Input type="password" name="password" value={input.password} onChange={ChangeEventHandler} placeholder="enter your password." />
+            <Input
+              type="password"
+              name="password"
+              value={input.password}
+              onChange={ChangeEventHandler}
+              placeholder="enter your password."
+            />
           </div>
           <div className="flex items-center justify-between">
-          <RadioGroup className="flex items-center gap-4 my-5">
+            <RadioGroup className="flex items-center gap-4 my-5">
               <div className="flex items-center space-x-2">
-             <Input type="radio" name="role" value="student" checked={input.role==="student"}  onChange={ChangeEventHandler} className="cursor-pointer"/>
+                <Input
+                  type="radio"
+                  name="role"
+                  value="student"
+                  checked={input.role === "student"}
+                  onChange={ChangeEventHandler}
+                  className="cursor-pointer"
+                />
                 <Label htmlFor="option-one">Student</Label>
               </div>
               <div className="flex items-center space-x-2">
-              <Input type="radio" name="role" value="recruiter" checked={input.role==="recruiter"} onChange={ChangeEventHandler} className="cursor-pointer"/>
+                <Input
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  checked={input.role === "recruiter"}
+                  onChange={ChangeEventHandler}
+                  className="cursor-pointer"
+                />
                 <Label htmlFor="option-two">Recruiter</Label>
               </div>
             </RadioGroup>
           </div>
-          <Button type="submit"className="w-full my-4">Login</Button>
-          <span className="text-sm">Don&apos;t have an account? <Link to='/signup' className="text-blue-400">Signup</Link></span>
+          {loading ? (
+            <Button className="w-full my-4">
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please wait{" "}
+            </Button>
+          ) : (
+            <Button type="submit" className="w-full my-4">
+              Login
+            </Button>
+          )}
+
+          <span className="text-sm">
+            Don&apos;t have an account?{" "}
+            <Link to="/signup" className="text-blue-400">
+              Signup
+            </Link>
+          </span>
         </form>
       </div>
     </div>
--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -7,7 +7,11 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
 import { USER_API_END_POINT } from "@/utils/constant";
-import { toast } from "sonner"
+import { toast } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "@/redux/authSlice";
+import { Loader2 } from "lucide-react";
+
 
 
 const Signup = () => {
@@ -19,8 +23,10 @@ const Signup = () => {
      role:"",
      file:"",
     });
-
+    const { loading } = useSelector((store) => store.auth);
+    const dispatch = useDispatch();
     const navigate = useNavigate();
+
     const onChangeEventHandler = (e)=>{
       setInput({...input, [e.target.name]:e.target.value})
     }
@@ -42,6 +48,7 @@ const Signup = () => {
         formData.append("file", input.file);
       }
       try {
+        dispatch(setLoading(true));
         const res = await axios.post(`${USER_API_END_POINT}/register`,formData, {
           headers:{
             'Content-Type':'multipart/form-data' 
@@ -56,6 +63,8 @@ const Signup = () => {
       } catch (error) {
         console.log(error)
         toast.error(error.response.data.message);
+      } finally {
+        dispatch(setLoading(false));
       }
     }
 
@@ -101,7 +110,14 @@ const Signup = () => {
               <Input accept="image/*"  type="file" onChange={ChangeFileHandler} className="cursor-pointer"/>
             </div>
           </div>
+          {loading ? (
+            <Button className="w-full my-4">
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please wait{" "}
+            </Button>
+          ) : (  
           <Button type="submit"className="w-full my-4">Signup</Button>
+          )}
           <span className="text-sm">Already have an account? <Link to='/login' className="text-blue-600">Login</Link></span>
         </form>
       </div>
