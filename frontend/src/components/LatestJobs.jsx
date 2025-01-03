1eba1da4184a4936e89faede5009735e62c549b5
Udgeet <bhatt8udgeet@gmail.com>
2025-01-03 14:09:57 +0530

    Building Home Page done!.

---
+++ frontend/src/components/LatestJobs.jsx
@@ -0,0 +1,19 @@
+import LatestJobCards from "./LatestJobCards";
+
+const randomJobs = [1,2,3,4,5,6,7,8];
+const LatestJobs = () => {
+
+  return (
+    <div className="max-w-7xl mx-auto my-20">
+      <h1 className="text-4xl font-bold"><span className="text-blue-500">Latest & Top </span> Job Openings</h1>
+      {/* //multiple job cards display */}
+      <div className="grid grid-cols-3 gap-4 my-5">
+      {
+        randomJobs.slice(0,6).map((item, index)=> <LatestJobCards/>)
+      }
+      </div>
+    </div>
+  )
+}
+
+export default LatestJobs;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,19 @@
+import CategoryCarousel from "@/components/CategoryCarousel"
+import Footer from "@/components/Footer"
+import HeroSection from "@/components/HeroSection"
+import LatestJobs from "@/components/LatestJobs"
 import Navbar from "@/components/shared/Navbar"
+
 
 
 const Home = () => {
   return (
     <div>
      <Navbar/>
+     <HeroSection/>
+     <CategoryCarousel/>
+     <LatestJobs/>
+     <Footer/>
     </div>
   )
 }
