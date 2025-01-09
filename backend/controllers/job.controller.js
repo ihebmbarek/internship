0d755523ab59f2446f323602158d104f0d56df86
Udgeet <bhatt8udgeet@gmail.com>
2025-01-09 20:31:47 +0530

    Persist Redux.

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -89,7 +89,7 @@ export const getAlljobs = async (req, res) => {
 export const getJobById = async(req,res) =>{
   try {
     const jobId = req.params.id;
-    const job = await Job.findById(jobId);
+    const job = await Job.findById(jobId).populate({path:'applications'});
     if(!job){
       return res.status(404).json({
         message:"Jobs not found.", success:false
